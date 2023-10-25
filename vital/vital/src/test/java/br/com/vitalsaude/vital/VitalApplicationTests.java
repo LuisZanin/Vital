e55f1fba6f6a9427478014717539fa8679af2198
LuisZanin <140721432+LuisZanin@users.noreply.github.com>
2023-10-25 19:27:56 -0300

    Aplicação VitalSaúde

---
+++ vital/vital/src/test/java/br/com/vitalsaude/vital/VitalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vitalsaude.vital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
